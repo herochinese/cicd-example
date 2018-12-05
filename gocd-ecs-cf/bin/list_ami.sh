b1883b30b63776207540e5f4ac5dbd7aa6f98f6d
CC <chuancc@88e9fe51b7a2.ant.amazon.com>
2018-12-06 00:36:32 +0800

    modified update policy.

--- gocd-ecs-cf/bin/list_ami.sh
+++ gocd-ecs-cf/bin/list_ami.sh
@@ -14,11 +14,10 @@ declare -a REGIONS=($(aws ec2 describe-regions --output json | jq '.Regions[].Re
 
 for r in "${REGIONS[@]}" ; do
     ami=$(aws ec2 describe-images --filters "Name=name,Values=${IMAGE_FILTER}" "Name=state,Values=available" --region ${r} --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
-    echo ${r}":" 
-    echo -e "\tAmazonLinux:   ${ami}"
+    echo ${r}":"
+    echo " AmazonLinux:   ${ami}"
 done
 
 #'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId'
 #aws ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.????????-x86_64-gp2' 'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId'
 #./list_ami.sh amzn2-ami-hvm-2.0.????????-x86_64-gp2
-
